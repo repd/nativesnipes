d94551804e8925cb2167d9a5d1a4a2fc27fe2971
unknown <willplayzmcyt@gmail.com>
2018-12-17 17:05:08 -0500

    NativeSnipes

---
+++ cmds/300s.js
@@ -0,0 +1,14 @@
+
+
+module.exports.run = async (bot,message,args) => {
+
+    message.channel.send("@here Next scrim begins in 5 minutes!");
+
+
+
+}
+
+module.exports.help = {
+    name: "300s"
+}
+
