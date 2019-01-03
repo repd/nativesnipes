a9f5fbc638487661cbcacf2972ed6a52ceef217e
repd <45952444+repd@users.noreply.github.com>
2019-01-02 23:00:56 -0500

    30s.js

--- cmds/30s.js
+++ cmds/30s.js
@@ -1,8 +1,14 @@
-
+const Discord = require('discord.js');
 
 module.exports.run = async (bot,message,args) => {
 
-    message.channel.send("@here Next scrim begins in 30 seconds!");
+    let startMessage = new Discord.RichEmbed()
+        .setTitle("Native Scrims")
+        .setDescription("Match starting in 30 seconds!")
+        .setColor("#c37d75")
+        .setFooter("repd");
+
+    message.channel.send({embed: startMessage});
 
 
 
