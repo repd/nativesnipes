d964fe751d52245625cbd99a56a8b502f5aee1cd
repd <45952444+repd@users.noreply.github.com>
2019-01-02 19:04:11 -0500

    Add files via upload

---
+++ cmds/cmds/60s.js
@@ -0,0 +1,14 @@
+
+
+module.exports.run = async (bot,message,args) => {
+
+    message.channel.send("Next scrim begins in 1 minute!");
+
+
+
+}
+
+module.exports.help = {
+    name: "60s"
+}
+
---
+++ cmds/cmds/now.js
@@ -0,0 +1,14 @@
+
+
+module.exports.run = async (bot,message,args) => {
+
+    message.channel.send("Next scrim beginning now!");
+
+
+
+}
+
+module.exports.help = {
+    name: "now"
+}
+
