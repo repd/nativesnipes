ae1a4fb0e5a915f454ebe228c6b86b0721909e9f
repd <45952444+repd@users.noreply.github.com>
2019-03-02 17:23:05 -0500

    now.js

--- cmds/now.js
+++ cmds/now.js
@@ -7,6 +7,7 @@ module.exports.run = async (bot,message,args) => {
         .setDescription("Match starting now!")
         .setColor("#c37d75")
         .setFooter("Announced by " + message.author.username);
+    startMessage.setTimestamp()
 
         message.channel.send({embed: startMessage});
 
